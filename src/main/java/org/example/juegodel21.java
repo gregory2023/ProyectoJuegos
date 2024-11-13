2f08213809a4627aad67db33625891da0b86503a
gregory2023 <gecanoram@gmail.com>
2024-11-13 21:12:05 +0100

    inicial local

---
+++ src/main/java/org/example/juegodel21.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class juegodel21 {
+
+    public  static void main (String [] args){
+
+        for (int i = 0; i <=5 ; i++) {
+
+            System.out.println(i);
+
+        }
+    }
+}
