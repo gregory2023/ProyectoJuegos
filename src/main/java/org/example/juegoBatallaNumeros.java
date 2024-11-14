46acc44009df3794eba00aafdd5a9a202e2ac2fc
gregory2023 <gecanoram@gmail.com>
2024-11-14 22:26:57 +0100

    juegoBatallaNumero

--- src/main/java/org/example/juegoBatallaNumeros.java
+++ src/main/java/org/example/juegoBatallaNumeros.java
@@ -1,4 +1,163 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class juegoBatallaNumeros {
+
+
+
+        public static Scanner Entrada;
+        public static int numeroJugador;
+        public static int numereroMaquina;
+        public static String nombre;
+        public static int puntos;
+        public static int partida;
+        public static int partidasGanadas;
+        public static int recompensa;
+
+
+
+        public static void main(String[] args) {
+            System.out.println("HOLA COMO TE LLAMAS ? ");
+            nombre = Entrada.nextLine();
+            System.out.println(" CADA INTENTO TIENE TRES RONDAS Y CADA INTENTO GANADO TIENE UN RECOMPENSA DE 400 PUNTOS ");
+            inicioJuego();
+        }
+
+        private static void inicioJuego() {
+            if (puntos >= 200) {
+                puntos -= partida;
+                jugador();
+            } else {
+                System.out.println("LO SIENTO YA NO TIENES MAS PUNTOS SUFICIENTES\n QUIERES RECARGAR (SI/NO)");
+                finJuego();
+            }
+
+        }
+
+        private static void jugador() {
+            for(int i = 1; i <= 3; ++i) {
+                System.out.println("----------------------------------------------------------------------\nturno de jugador");
+                numeroJugador = (int)(Math.random() * 12.0) + 1;
+                numereroMaquina = (int)(Math.random() * 12.0) + 1;
+                System.out.println(nombre + " TU NUMERO ASIGNADO ES  " + numeroJugador);
+                System.out.println(" maquina TU NUMERO ASIGNADO ES  " + numereroMaquina);
+                boolean poderUtilizado = false;
+
+                do {
+                    System.out.println(" QUIERES UTILIZAR UN PODER  (SI/NO) ? \nRECUERDA SOLO TIENES UN PODER POR RONDA ");
+                    String desicionPoder = Entrada.nextLine();
+                    if (!desicionPoder.equals("si") && !desicionPoder.equals("SI")) {
+                        if (desicionPoder.equals("no") || desicionPoder.equals("NO")) {
+                            System.out.println("VALE , JUEGAS SIN PODER");
+                        }
+                    } else {
+                        System.out.println("GENIAL AQUI TIENES TUS PODERES \n1.DIVIDIR VALOR CONTRINCANTE \n2.DOBLAR NUMERO \n3.ROBAR NUMERO CONTRINCANTE");
+                        int opc = Entrada.nextInt();
+                        Entrada.nextLine();
+                        switch (opc) {
+                            case 1:
+                                System.out.println("IMPORTANTE !! SI EL NUMERO CONTRINCANTE ES MENOR O IGUAL A 5 PIERDES YA QUE NO SE PUEDE DIVIDIR ESTA CANTIDAD \nCONTINUAS- DIGITE UN NUMERO (1.SI/2.NO) ");
+                                int desicionPoderDividir = Entrada.nextInt();
+                                Entrada.nextLine();
+                                if (desicionPoderDividir == 1) {
+                                    numereroMaquina /= 2;
+                                    System.out.println("PODER APLICADO");
+                                    poderUtilizado = true;
+                                } else {
+                                    System.out.println("VALE, VUELVES AL MENU INICIAL :) ");
+                                }
+                                break;
+                            case 2:
+                                numeroJugador *= 2;
+                                System.out.println(" PODER APLICADO , NUMERO POTENCIADO ES IGUAL A :  " + numeroJugador);
+                                poderUtilizado = true;
+                                break;
+                            case 3:
+                                numeroJugador = numereroMaquina;
+                                System.out.println("PODER APLICADO, AHORA TU NUMERO ROBADO ES :" + numereroMaquina);
+                                poderUtilizado = true;
+                                break;
+                            default:
+                                System.out.println("VALOR INCORRECTO");
+                        }
+                    }
+                } while(!poderUtilizado);
+
+                maquina();
+                operacion();
+            }
+
+            sumaPuntos();
+        }
+
+        public static void maquina() {
+            System.out.println("----------------------------------------------------------------------\nturno de maquina");
+            int decision = (int)(Math.random() * 3.0) + 1;
+            switch (decision) {
+                case 1:
+                    System.out.println("MAQUINA A ELEGIDO EL PODER DE DIVIDIR TU NUMERO ");
+                    numeroJugador /= 2;
+                    break;
+                case 2:
+                    if (numereroMaquina <= 10) {
+                        System.out.println("MAQUINA A ELEGIDO  DOBLAR SU NUMERO  ");
+                        numereroMaquina *= 2;
+                        break;
+                    }
+                case 3:
+                    System.out.println("MAQUINA A ELEGIDO ROBAR TU NUMERO ");
+                    numereroMaquina = numeroJugador;
+                    break;
+                default:
+                    System.out.println("ERROR CAPA 8");
+            }
+
+        }
+
+        public static void operacion() {
+            if (numeroJugador > numereroMaquina) {
+                ++partida;
+                System.out.println(nombre + "tu numero es " + numeroJugador);
+                System.out.println("maquina tu numeroe es " + numereroMaquina);
+                System.out.println("GANA " + nombre);
+            } else if (numeroJugador == numereroMaquina) {
+                System.out.println(nombre + "tu numero es " + numeroJugador);
+                System.out.println("maquina tu numeroe es " + numereroMaquina);
+                System.out.println("empate");
+            } else {
+                System.out.println(nombre + "tu numero es " + numeroJugador);
+                System.out.println("maquina tu numeroe es " + numereroMaquina);
+                System.out.println("gana maquina ");
+            }
+
+        }
+
+        public static void sumaPuntos() {
+            if (partidasGanadas > 2) {
+                puntos += 400;
+            } else {
+                System.out.println("RONDA PERDIDA ");
+                System.out.println("quieres volver a jugar (si/no");
+                String desicion = Entrada.nextLine();
+                if (!desicion.equals("si") && !desicion.equals("SI")) {
+                    finJuego();
+                } else {
+                    inicioJuego();
+                }
+            }
+
+        }
+
+        public static void finJuego() {
+            System.out.println("gracias ");
+        }
+
+        static {
+            Entrada = new Scanner(System.in);
+            puntos = 200;
+            partida = 200;
+            partidasGanadas = 0;
+        }
 }
+
--- src/main/java/org/example/juegodel21.java
+++ src/main/java/org/example/juegodel21.java
@@ -1,13 +1,120 @@
 package org.example;
-
+import java.util.Scanner;
 public class juegodel21 {
 
     public  static void main (String [] args){
 
-        for (int i = 0; i <=5 ; i++) {
+        Scanner Entrada = new Scanner(System.in);
 
-            System.out.println(i);
+        int numeroMaquina=0;
+        boolean bandera =true ;
+        int sumaMaquina =0;
+        int totalGanar=21;
+        int sumaPerder;
 
+
+        //variables para calcular la probabilidad
+        int numerosPosibles=11;
+        float probabilidad=0;
+        int numerosPeligrosos=0;
+
+
+        int numeroUsuario = 0;
+        int suma = 0;
+        String decision;         // Almacena la decisión del jugador ("si" o "no").
+        int contador = 1;
+
+
+       numeroMaquina=(int)(Math.random()*11)+1;
+       sumaMaquina=numeroMaquina;
+       numeroUsuario = (int)(Math.random() * 11) + 1;
+
+
+       System.out.println("INICIEMOS EL JUEGO DEL 21 !!");
+       System.out.println("Primera carta tiene un valor de " + numeroMaquina);
+
+
+
+        sumaPerder = totalGanar-sumaMaquina;
+
+
+        //calcular la probabilidad
+
+        for (int i = 1; i <=11 ; i++) {
+
+            if (sumaMaquina + i >21){
+                numerosPeligrosos++;
+            }
         }
+        System.out.println(numerosPeligrosos);
+
+        probabilidad =((float)numerosPeligrosos/(float) numerosPosibles )*100;
+
+
+        System.out.println(probabilidad);
+
+
+
+
+
+       /* System.out.println("Primera carta tiene un valor de " + numeroUsuario);
+
+        // adicion a suma para el control de las cartas
+        suma = suma + numeroUsuario;
+
+        // Bucle que permite al jugador pedir más cartas o plantarse.
+        do {
+
+
+            System.out.println("¿Quieres otra carta? (si/no)");
+            decision = Entrada.nextLine().toLowerCase();
+
+            while (!decision.equals("si") && !decision.equals("no")){
+                System.out.println("ingrese 'si' o 'no' por favor ");
+                decision = Entrada.nextLine().toLowerCase();
+            }
+
+            // Si el jugador decide pedir otra carta:
+            if (decision.equals("si")) {
+
+                numeroUsuario = (int)(Math.random() * 11) + 1;
+                suma = suma + numeroUsuario;
+
+
+                System.out.println("El valor de la siguiente carta es de " + numeroUsuario);
+                System.out.println("Llevas un valor de " + suma);
+
+                // Incrementa el contador de cartas solicitadas.
+                contador++;
+            } else {
+
+                decision = "no";
+            }
+
+            // Si el jugador ha alcanzado exactamente 21 puntos, gana automáticamente.
+            if (suma == 21) {
+                System.out.println("¡Has ganado!");
+                System.out.println("Suma total: " + suma);
+                break;
+            }
+
+
+        } while (suma <= 21 && decision.equals("si")); // El bucle se repite mientras la suma no supere 21 y el jugador quiera otra carta.
+
+
+        if (suma >= 22) {
+
+            System.out.println("Lo siento, has perdido.");
+            System.out.println("Perdiste en la carta número " + contador);
+        } else {
+            // Si el jugador se planta sin pasarse, muestra el total y la cantidad de cartas solicitadas.
+            System.out.println("Te plantaste con un valor de " + suma);
+            System.out.println("Pediste un total de " + contador + " cartas.");
+        }
+*/
     }
+
+
 }
+
+
